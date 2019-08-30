8097e6988ec9ee38df6d6e2715954219cf704e01
Ray <Ray@152.23.235.169>
2019-08-30 12:42:29 -0400

    Revision

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -6,9 +6,9 @@ public class A1Adept {
 
 	public static void main(String[] args) {
 		
-		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		Scanner sc = new Scanner(System.in);
+		
+		
 		
 	}
 }

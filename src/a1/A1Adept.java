b2ec3e4339440580cbf92b3d992755bb65206df2
Ray <Ray@152.23.235.169>
2019-08-30 14:29:22 -0400

    1

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -13,6 +13,7 @@ public class A1Adept {
 		for (int i=0; i<=ItemNumber-1 ; i++) {
 			ItemNames[i]            = sc.next();
 			ItemPrices [i]       = sc.nextDouble();
+			System.out.println(ItemNames[i]+" "+ ItemPrices[i]);
 		}
 
 		int CustomerNumber    = sc.nextInt();
@@ -24,13 +25,18 @@ public class A1Adept {
 			LastName [i]      = sc.next();
 			int ItemNumber2   = sc.nextInt();
 			int []    ItemNumber3 = new int[ItemNumber2];
-			String [] ItemName2   = new String[ItemNumber2];	
+			String [] ItemName2   = new String[ItemNumber2];
+			System.out.println(FirstName[i]+" "+LastName[i]);
 			for (int m=0; m<=ItemNumber2-1 ; m++) {
 				ItemNumber3[m] = sc.nextInt();
 				ItemName2 [m]  = sc.next();
+				for (int b=0; b<=ItemNumber-1; b++) {
+					if (ItemName2 [m] == ItemNames [b]) {
+						double TotalPrice = ItemNumber3[m] * ItemPrices[b];
+						System.out.println(ItemName2[m] + " " + TotalPrice);
+						break;}
+						}
+					}
 				}
-		}         
-				
-		
+			}	       						
 	}
-}

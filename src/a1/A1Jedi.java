24967a4eed6f18ad38050cdbfd19643bcf7f10be
Ray <Ray@192.168.0.7>
2019-08-31 13:34:43 -0400

    Complete of Jedi

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -20,6 +20,9 @@ public class A1Jedi {
 		int CustomerNumber    = sc.nextInt();
 		String [] FirstName   = new String[CustomerNumber];
 		String [] LastName    = new String[CustomerNumber];
+		
+		int    [] CustomerNumberIndicator = new int [ItemNumber];
+		int    [] ItemNumberIndicator     = new int [ItemNumber];
 
 		for (int i=0; i<=CustomerNumber-1 ; i++) {
 			FirstName[i]      = sc.next();
@@ -27,7 +30,6 @@ public class A1Jedi {
 			int ItemNumberBought   = sc.nextInt();
 			int []    ItemNumberEachBought = new int[ItemNumberBought];
 			String [] ItemNameBought   = new String[ItemNumberBought];
-			double [] TotalPrices = new double[ItemNumberBought];
 			for (int m=0; m<=ItemNumberBought-1 ; m++) {
 				ItemNumberEachBought[m] = sc.nextInt();
 				ItemNameBought [m]  = sc.next();
@@ -38,8 +40,23 @@ public class A1Jedi {
 					boolean result = ItemName[n].equals(ItemNameBought[j]);
 					if (result) { ItemNumber4EveryItem [n] += ItemNumberEachBought[j];}					
 					}
-			System.out.println(ItemName[n]+" "+ItemNumber4EveryItem[n]);
+				if (ItemNumber4EveryItem[n]==0) {
+					CustomerNumberIndicator[n]+=0;
+					ItemNumberIndicator[n]+=0;
+					}
+				else {
+					CustomerNumberIndicator[n]+=1;
+					ItemNumberIndicator[n]+=ItemNumber4EveryItem[n];}
 			}
+			
 	    }
+		
+		
+		for (int i=0; i<=ItemNumber-1;i++) {
+			if (CustomerNumberIndicator[i]==0) {
+				System.out.println("No customers bought "+ItemName[i]);
+			}
+			else {System.out.println(CustomerNumberIndicator[i]+" customers bought "+ItemNumberIndicator[i]+" "+ItemName[i]);}
+		}
   }
 }

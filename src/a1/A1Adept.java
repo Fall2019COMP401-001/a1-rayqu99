cc1b21887d5450c1ca8e6144a3afe2b353678ed1
Ray <Ray@152.23.235.169>
2019-08-30 22:07:30 -0400

    Compelete of Adept

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,5 +1,6 @@
 package a1;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class A1Adept {
@@ -13,12 +14,12 @@ public class A1Adept {
 		for (int i=0; i<=ItemNumber-1 ; i++) {
 			ItemNames[i]            = sc.next();
 			ItemPrices [i]       = sc.nextDouble();
-			System.out.println(ItemNames[i]+" "+ ItemPrices[i]);
 		}
 
 		int CustomerNumber    = sc.nextInt();
 		String [] FirstName   = new String[CustomerNumber];
 		String [] LastName    = new String[CustomerNumber];
+		double [] FinalPrice  = new double[CustomerNumber];
 
 		for (int i=0; i<=CustomerNumber-1 ; i++) {
 			FirstName[i]      = sc.next();
@@ -26,7 +27,6 @@ public class A1Adept {
 			int ItemNumber2   = sc.nextInt();
 			int []    ItemNumber3 = new int[ItemNumber2];
 			String [] ItemName2   = new String[ItemNumber2];
-			System.out.println(FirstName[i]+" "+LastName[i]);
 			double [] TotalPrices = new double[ItemNumber2];
 			for (int m=0; m<=ItemNumber2-1 ; m++) {
 				ItemNumber3[m] = sc.nextInt();
@@ -35,10 +35,44 @@ public class A1Adept {
 					    boolean result = ItemName2[m].equals(ItemNames[i1]);
 					    if (result) {TotalPrices[m] = ItemNumber3[m] * ItemPrices[i1];}
 				         }}
-			double FinalPrice = 0;
 			for (int n=0; n<=ItemNumber2-1;n++) {
-					FinalPrice += TotalPrices[n];}
-				System.out.println(FinalPrice);			
-	    }	       						
+					FinalPrice[i] += TotalPrices[n];}			
+	    }
+		DecimalFormat df = new DecimalFormat("0.00");
+		
+		double MaxFP = FinalPrice[0];
+		String MaxFirstName = FirstName[0];
+		String MaxLastName =  LastName[0];
+		for (int j=0; j<CustomerNumber-1; j++) {
+			if (FinalPrice[j]<FinalPrice[j+1]) {
+				MaxFP = FinalPrice[j+1];
+				MaxFirstName = FirstName[j+1];
+				MaxLastName = LastName[j+1];
+			}
+		}
+		String MaxFP2 =df.format(MaxFP);
+		System.out.println("Biggest: "+MaxFirstName+" "+MaxLastName+" ("+MaxFP2+")");
+		
+		double MinFP = FinalPrice[0];
+		String MinFirstName = FirstName[0];
+		String MinLastName =  LastName[0];
+		for (int j=0; j<CustomerNumber-1; j++) {
+			if (FinalPrice[j]>FinalPrice[j+1]) {
+				MinFP = FinalPrice[j+1];
+				MinFirstName = FirstName[j+1];
+				MinLastName = LastName[j+1];
+			}
+		}
+		String MinFP2 =df.format(MinFP);
+		System.out.println("Smallest: "+MinFirstName+" "+MinLastName+" ("+MinFP2+")");
+			
+		double SumPrice = 0;
+		for (int k=0; k<=CustomerNumber-1; k++) {
+			 SumPrice += FinalPrice[k];
+		}
+		double average =SumPrice / CustomerNumber;
+		String average2 =df.format(average);
+		System.out.println("Average: "+average2);
+		sc.close();
 	}
 }
